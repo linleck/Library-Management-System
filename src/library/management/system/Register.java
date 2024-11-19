b752331e8e5b87ff4ef8f4e6c9d5f2f85e4e74c5
linleck <linleckkyalsin26@gmail.com>
2024-11-19 20:15:06 +0630

    member register

---
+++ src/library/management/system/Register.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.management.system;
+
+/**
+ *
+ * @author linleckkyalsin
+ */
+public class Register {
+    public static void main(String[] args) {
+    RegisterationForm registerForm= new RegisterationForm();
+    registerForm.setVisible(true);
+    registerForm.pack();
+    registerForm.setLocationRelativeTo(null);
+    }
+}
